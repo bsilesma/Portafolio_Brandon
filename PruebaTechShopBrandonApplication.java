4879a3fd75f68e1f84adb4d23df26be950547328
bsilesma <silesmatab@gmail.com>
2026-05-31 20:51:33 -0600

    Trabajo semana 1-2
    Saludos, este es el ejemplo visto en semana 1 y el ejemplo de semana 1 funcionando con netbeans en semana 2

---
+++ PruebaTechShopBrandonApplication.java
@@ -0,0 +1,13 @@
+package pruebaTechShop.Brandon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTechShopBrandonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTechShopBrandonApplication.class, args);
+	}
+
+}
